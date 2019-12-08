d1d5dda72f2c71f2ba7bb1082b75cb5e0a822a9a
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-12-08 00:33:59 -0500

    Moved HTML processing into utility function.

--- src/retroproxy/routes.py
+++ src/retroproxy/routes.py
@@ -4,11 +4,9 @@ import requests
 import re
 from urllib.parse import urlparse, urlunsplit
 from flask import current_app, render_template, request, abort
-from bs4 import BeautifulSoup
 from . import util
 
 PORT_PATTERN = re.compile( r'(.*)(:[0-9]*)$' )
-ARCHIVE_PATTERN = re.compile( r'^((https?://web.archive.org)?/web/[0-9a-z_]*/)' )
 
 @current_app.route( '/', defaults={'path': ''} )
 @current_app.route( '/<path:path>' )
@@ -34,7 +32,7 @@ def retroproxy_root( path ):
         url.query,
         url.fragment ) )
 
-    print( url )
+    logger.info( url )
 
     # Submit the request to archive.org.
     archive_url = 'https://web.archive.org/web/{}/{}'.format(
@@ -44,38 +42,5 @@ def retroproxy_root( path ):
     if 404 == response.status_code:
         abort( 404 )
 
-    soup = BeautifulSoup( response.text, 'html.parser' )
-    for a in soup.findAll( 'a' ):
-        a['href'] = ARCHIVE_PATTERN.sub( '', a['href'] )
-
-        # Add our listening port to the netloc.
-        a['href'] = util.fix_netloc_port( a['href'], url_port )
-
-    # Fix for static CSS.
-    # TODO: Should these just be stripped?
-    for link in soup.findAll( 'link' ):
-        if link['href'].startswith( '/_static/' ):
-            link['href'] = 'https://web.archive.org{}'.format( link['href'] )
-
-    # Fix for images relative to web.archive.org.
-    for img in soup.findAll( 'img' ):
-        if img['src'].startswith( '/web/' ):
-            img['src'] = 'https://web.archive.org{}'.format( img['src'] )
-
-    # Fix for optional base tag.
-    for base in soup.findAll( 'base' ):
-        base['href'] = ARCHIVE_PATTERN.sub( '', base['href'] )
-
-        base['href'] = util.fix_netloc_port( base['href'], url_port )
-
-    # Strip out archive modernity hacks.
-    for script in soup.findAll( 'script', ):
-        if 'src' in script.attrs and (
-        'client-rewrite.js' in script.attrs['src'] or \
-        'wbhack.js' in script.attrs['src'] ) or \
-        'wbhack.' in script.text or \
-        'WB_wombat_Init' in script.text:
-            script.extract()
-
-    return str( soup )
+    return util.process_html_links( response.text, url_port=url_port )
 
--- src/retroproxy/util.py
+++ src/retroproxy/util.py
@@ -1,9 +1,13 @@
 
+import re
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urlunsplit
+
+ARCHIVE_PATTERN = re.compile( r'^((https?://web.archive.org)?/web/[0-9a-z_]*/)' )
 
 def fix_netloc_port( url_in, url_port ):
 
-    ''' Given a URL, insder the given listening port next to the host. '''
+    ''' Given a URL, insert the given listening port next to the host. '''
 
     nl_url = urlparse( url_in )
     if nl_url.netloc:
@@ -14,4 +18,48 @@ def fix_netloc_port( url_in, url_port ):
 
     else:
         return url_in
+
+def process_html_links( text, url_port=80 ):
+
+    soup = BeautifulSoup( text, 'html.parser' )
+    for a in soup.findAll( 'a' ):
+        a['href'] = ARCHIVE_PATTERN.sub( '', a['href'] )
+
+        # Add our listening port to the netloc.
+        a['href'] = fix_netloc_port( a['href'], url_port )
+
+    # Fix for static CSS.
+    # TODO: Should these just be stripped?
+    for link in soup.findAll( 'link' ):
+        try:
+            if link['href'].startswith( '/_static/' ):
+                link['href'] = \
+                    'https://web.archive.org{}'.format( link['href'] )
+        except Exception as e:
+            logger.warning( e )
+
+    # Fix for images relative to web.archive.org.
+    for img in soup.findAll( 'img' ):
+        if img['src'].startswith( '/web/' ):
+            img['src'] = 'https://web.archive.org{}'.format( img['src'] )
+
+    # Fix for optional base tag.
+    for base in soup.findAll( 'base' ):
+        try:
+            base['href'] = ARCHIVE_PATTERN.sub( '', base['href'] )
+
+            base['href'] = fix_netloc_port( base['href'], url_port )
+        except Exception as e:
+            logger.warning( e )
+
+    # Strip out archive modernity hacks.
+    for script in soup.findAll( 'script', ):
+        if 'src' in script.attrs and (
+        'client-rewrite.js' in script.attrs['src'] or \
+        'wbhack.js' in script.attrs['src'] ) or \
+        'wbhack.' in script.text or \
+        'WB_wombat_Init' in script.text:
+            script.extract()
+
+    return str( soup )
 
